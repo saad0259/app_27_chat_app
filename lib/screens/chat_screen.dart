a5322f5ac59c7cfbf5a2d7cf0922f97dbd6d9b75
Muhammad Saad Shafiq <saadshafiq259@gmail.com>
2021-10-15 07:55:18 +0500

    firebase firestore integration

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('This works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/N5QWyitGiVMSTex68vPM/messages')
+              .snapshots()
+              .listen((data) {
+            print(data);
+          });
+        },
+      ),
+    );
+  }
+}
